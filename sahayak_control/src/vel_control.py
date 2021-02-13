434f69bde17ebec443923fe912d7e69edcd6a7d4
Kush0301 <kushagra2000@gmail.com>
2021-02-14 00:14:47 +0530

    Teleoperation with the help of arrow keys

--- sahayak_control/src/vel_control.py
+++ sahayak_control/src/vel_control.py
@@ -3,6 +3,9 @@ import rospy
 from std_msgs.msg import Float64
 from sensor_msgs.msg import JointState
 import numpy as np
+from pynput import keyboard
+from pynput.keyboard import Listener, Key
+
 PI = np.pi
 
 class sahayak:
@@ -32,19 +35,78 @@ class sahayak:
         self.start = rospy.get_rostime()
         
         while not rospy.is_shutdown():
-            self.now = rospy.get_rostime()
+            with keyboard.Listener(on_press=bot.on_press,on_release=on_release) as listener:
+                listener.join()
+       
+    def on_press(self,key):
+        try:
+            if key == Key.up:
+                print('Moving Forward')
+                self.now = rospy.get_rostime()
+                              
+                self.joint_pub['joint1'].publish(30.0*2*PI/60) # RPM
+                self.joint_pub['joint2'].publish(-30.0*2*PI/60) # RPM
+                self.joint_pub['joint3'].publish(30.0*2*PI/60) # RPM
+                self.joint_pub['joint4'].publish(-30.0*2*PI/60) # RPM
+                
+                self.rate.sleep()
+            elif key== Key.left:
+                print('Moving left')
+                
+                self.now = rospy.get_rostime()
+                              
+                self.joint_pub['joint1'].publish(30.0*2*PI/60) # RPM
+                self.joint_pub['joint2'].publish(30.0*2*PI/60) # RPM
+                self.joint_pub['joint3'].publish(30.0*2*PI/60) # RPM
+                self.joint_pub['joint4'].publish(30.0*2*PI/60) # RPM
+                
+                self.rate.sleep()       
+            elif key==Key.right:
+                print('Moving right')
+                
+                self.now = rospy.get_rostime()
+                              
+                self.joint_pub['joint1'].publish(-30.0*2*PI/60) # RPM
+                self.joint_pub['joint2'].publish(-30.0*2*PI/60) # RPM
+                self.joint_pub['joint3'].publish(-30.0*2*PI/60) # RPM
+                self.joint_pub['joint4'].publish(-30.0*2*PI/60) # RPM
+                
+                self.rate.sleep()              
+            elif key==Key.down:
+                print('Moving back')
+                self.now = rospy.get_rostime()
+                              
+                self.joint_pub['joint1'].publish(-30.0*2*PI/60) # RPM
+                self.joint_pub['joint2'].publish(30.0*2*PI/60) # RPM
+                self.joint_pub['joint3'].publish(-30.0*2*PI/60) # RPM
+                self.joint_pub['joint4'].publish(30.0*2*PI/60) # RPM
+                
+                self.rate.sleep()
+            elif key==Key.enter:
+                print('Stop')
+                self.now = rospy.get_rostime()
+                              
+                self.joint_pub['joint1'].publish(0.0) # RPM
+                self.joint_pub['joint2'].publish(0.0) # RPM
+                self.joint_pub['joint3'].publish(0.0) # RPM
+                self.joint_pub['joint4'].publish(0.0) # RPM
+                
+                self.rate.sleep()
 
-            # for joint_indx in range(1, 5):                                
-            self.joint_pub['joint1'].publish(-30.0*2*PI/60) # RPM
-            self.joint_pub['joint2'].publish(30.0*2*PI/60) # RPM
-            self.joint_pub['joint3'].publish(-30.0*2*PI/60) # RPM
-            self.joint_pub['joint4'].publish(30.0*2*PI/60) # RPM
-            
-            self.rate.sleep()            
+            elif key==Key.esc:
+                rospy.signal_shutdown("Shutting down")
+            else:
+                print('Invalid key, robot retains previous command')
+        except AttributeError:
+            print('Invalid Key, robot retains previous command')
 
-            # # spin() simply keeps python from exiting until this node is stopped
-            # rospy.spin()
+def on_release(key):
+    print(key) 
+    if key == keyboard.Key.esc:
+        return False
 
+# Collect events until released
+           
 if __name__ == '__main__':
     bot = sahayak()
     bot.move()
